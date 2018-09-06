ecc341e4a7cd5f0e1add7a36d3b49e7fb18c28dc
oscar orellana <saiphcita@gmail.com>
2018-09-06 01:04:54 -0600

    generating report

--- src/Components/Tools/DataBase.js
+++ src/Components/Tools/DataBase.js
@@ -21,3 +21,26 @@ var refChatRoom = db.ref("ChatRoom")
 var dbUser = db
 
 export { refGeneralCategory, refGeneralPosts, refAllUsers, refChatRoom, dbUser}
+
+//generating report
+var refReport = db.ref("Report");
+refAllUsers.on("value", (snapshot) => {
+    let users = snapshot.val();
+    var infoCosole = [];
+    for (let i = 0; i < users.length; i++) {
+        var seleccted = [];
+        for (let j = 0; j < users[i].PostAndCategory.Post.length; j++) {
+            if(users[i].PostAndCategory.Post[j].category !== "Select Category"){
+                seleccted.push(users[i].PostAndCategory.Post[j].category)
+            };
+        };
+        var infoArray = {
+            "1-Worker": users[i].UserInfo.Username,
+            "2-Post": users[i].PostAndCategory.Post,
+            "3-Selected": seleccted.length+ " of "+users[i].PostAndCategory.Post.length,
+            "4-State": users[i].UserState
+        };
+        infoCosole.push(infoArray);
+    };
+    refReport.set(infoCosole)
+});
