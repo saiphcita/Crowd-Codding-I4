763555acead4b626338aa44c64271ef46c7021b3
oscar orellana <saiphcita@gmail.com>
2018-09-10 18:38:12 -0600

    f

--- src/Components/Tools/ListCategory.js
+++ src/Components/Tools/ListCategory.js
@@ -21,14 +21,16 @@ class ListCategory extends React.Component {
     return (
         <div className="DivDefinition2">
           <div className="titleList2">
-            <li style={{width:"12%", maxWidth:"12%"}}>category</li>
-            <li style={{width:"88%", maxWidth:"88%"}}>Description</li>
+            <li style={{width:"15%", maxWidth:"15%"}}>category</li>
+            <li style={{width:"47%", maxWidth:"47%"}}>Description</li>
+            <li style={{width:"38%", maxWidth:"38%"}}>Example</li>
           </div>
           {this.state.category.map((val, ind) =>{
             return (
               <div key={ind} className="Clist">
-                <li key={ind} style={{width:"12%", maxWidth:"12%"}}>{val.categoryName}</li>
-                <li key={val.post} style={{width:"88%", maxWidth:"88%"}}>{val.categoryDefinition}</li>
+                <li key={ind} style={{width:"15%", maxWidth:"15%"}}>{val.categoryName}</li>
+                <li key={val.post} style={{width:"47%", maxWidth:"47%"}}>{val.categoryDefinition}</li>
+                <li style={{width:"38%", maxWidth:"38%"}}>{val.categoryExample}</li>
               </div>
             )
           })}
